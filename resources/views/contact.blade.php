caf017c2ac89eb6c8d1a56fc70fe21ba8380f07a
Karol Henriques <karoljares@gmail.com>
2023-11-20 16:14:23 +0000

    Profile images updated (Karol, Pedro and Inês)

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'CONTACT')
+
+@section('content')
+    @include('sections.contact_form')
+@endsection
---
+++ resources/views/sections/about_us.blade.php
@@ -0,0 +1,46 @@
+<section class="bg-light py-5" style= "max-height: 60%;">
+    <div class="container px-5" >
+        <div class="text-center mb-5">
+            <div class="feature bg-primary bg-gradient-primary-to-secondary text-white rounded-3 mb-3"><i class="bi bi-code" ></i></div>
+            <h1 class="fw-bolder">This is my city Team</h1>
+        </div>
+        <div class="row gx-5 justify-content-center" >
+            <div class="col-md-4 d-flex flex-column align-items-center">
+                <div class="profile bg-gradient-primary-to-secondary" style="max-width: 40%; max-height: 40%;">
+                    <img class="profile-img" src="karol_profile-removebg.png" alt="Image 2" style="max-width: 100%; height: auto;" />
+                </div>
+                <ul class="list-unstyled text-muted mt-2 mb-0">
+                    <li>Karol Henriques</li>
+                    <li>3º ano</li>
+                    <li>UFP</li>
+                    <li>karolhenriques@timcity.com</li>
+                    <li class="badge bg-gradient-primary-to-secondary text-white mb-4">Design &middot; Code &middot; Software engineering </li>
+                </ul>
+            </div>
+            <div class="col-md-4 d-flex flex-column align-items-center">
+                <div class="profile bg-gradient-primary-to-secondary" style="max-width: 40%; max-height: 40%;">
+                    <img class="profile-img" src="Pedro_profile-removebg.png" alt="Image 2" style="max-width: 100%; height: auto;" />
+                </div>
+                <ul class="list-unstyled text-muted mt-2 mb-0">
+                    <li>Pedro Marques</li>
+                    <li>3º ano</li>
+                    <li>UFP</li>
+                    <li>pedromarques@timcity.com</li>
+                    <li class="badge bg-gradient-primary-to-secondary text-white mb-4">Code &middot; Software engineering </li>
+                </ul>
+            </div>
+            <div class="col-md-4 d-flex flex-column align-items-center">
+                <div class="profile bg-gradient-primary-to-secondary" style="max-width: 40%; max-height: 40%;">
+                    <img class="profile-img" src="Ines_profile-removebg.png" alt="Image 2" style="max-width: 100%; height: auto;" />
+                </div>
+                <ul class="list-unstyled text-muted mt-2 mb-0">
+                    <li>Maria Inês Gomes</li>
+                    <li>3º ano</li>
+                    <li>UFP</li>
+                    <li>minesgomes@timcity.com</li>
+                    <li class="badge bg-gradient-primary-to-secondary text-white mb-4">Software engineering </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
